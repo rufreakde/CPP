d7060933b5adb35b70dd3a2add88cfb610add7fe
Habib Gahbiche <habibgahbiche@gmail.com>
2017-11-27 20:55:31 +0100

    aufgabe 1 fertig!! no errors, no warnings

--- Ex5_Chrispens_Gahbiche/atomics.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by Habib Gahbiche on 26/11/2017.
-//
-
-#ifndef EX5_CHRISPENS_GAHBICHE_ATOMICS_H
-#define EX5_CHRISPENS_GAHBICHE_ATOMICS_H
-
-#endif //EX5_CHRISPENS_GAHBICHE_ATOMICS_H
